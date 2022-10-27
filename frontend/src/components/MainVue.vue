c505d02ccf0a6111f4314d24e083d3b61c62c90f
e759ksh <e759ksh@naver.com>
2022-10-27 13:02:37 +0900

    프론트 json 시각화

--- frontend/src/components/MainVue.vue
+++ frontend/src/components/MainVue.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <p>main</p>
+    <div v-for="user in this.$store.state.user.users" :key="user">
+      <p>{{ user.username }}</p>
+      <small>{{ `${user.name.first} ${user.name.last}` }}</small>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      user: [],
-    };
+    return {};
   },
   created() {
     this.$store.dispatch("SET_USER_DATA");
